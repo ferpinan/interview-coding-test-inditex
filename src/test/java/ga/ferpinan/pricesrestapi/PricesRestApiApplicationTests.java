e974a5a051032f64a8632208e6571c96fe862d3a
Andoni <andoni2222@gmail.com>
2020-11-13 12:34:02 +0100

    [PRA-1] Create Spring boot App

---
+++ src/test/java/ga/ferpinan/pricesrestapi/PricesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.ferpinan.pricesrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
