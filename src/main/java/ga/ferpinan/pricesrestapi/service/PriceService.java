a95403b9de6f2bf0c8d0607e829066dd9e16e936
Andoni <andoni2222@gmail.com>
2020-11-14 23:47:26 +0100

    [PRA-4] Log config

--- src/main/java/ga/ferpinan/pricesrestapi/service/PriceService.java
+++ src/main/java/ga/ferpinan/pricesrestapi/service/PriceService.java
@@ -4,6 +4,7 @@ import ga.ferpinan.pricesrestapi.exception.PriceNotFoundException;
 import ga.ferpinan.pricesrestapi.model.Price;
 import ga.ferpinan.pricesrestapi.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,15 +17,19 @@ import java.util.List;
  */
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class PriceService {
 
 	private final PriceRepository priceRepository;
 
 	@Transactional
 	public Price findPriorPrice(Long brandId, Long productId, LocalDateTime dateBetweenStartAndEndDate) {
+        log.debug("PriceService::findPriorPrice -> Called with brandId {}, productId {} and date {}", brandId, productId, dateBetweenStartAndEndDate);
         List<Price> priorPriceList = priceRepository.findPriorPriceList(brandId, productId, dateBetweenStartAndEndDate, PageRequest.of(0, 1));
         if(priorPriceList.isEmpty()){
-            throw new PriceNotFoundException(String.format("findByPriceList: Price for brand %s, product %s for date %s not found", brandId, productId, dateBetweenStartAndEndDate));
+            String errorMessage = String.format("PriceService::findPriorPrice -> Price for brand %s, product %s for date %s not found", brandId, productId, dateBetweenStartAndEndDate);
+            log.error(errorMessage);
+            throw new PriceNotFoundException(errorMessage);
         }
         return priorPriceList.get(0);
 	}
